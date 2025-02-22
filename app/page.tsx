8a9c3ecced589859652a1e74096815a2d7373b00
Simon Tenedero <steneder@Simons-Air.wlu.ca>
2025-02-22 18:40:36 -0500

    hi

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import { Bold } from "lucide-react";
 export default function Home() {
   return (
     <div className="relative">
-      <img src="/press_space_bar.svg" className="absolute top-20 left-200" width="20%"></img>
-      <img src="/hover_over_cards.svg" className="absolute bottom-10 left-170" width="20%"></img>
+      <img src="/press_space_bar.svg" className="absolute top-35 left-200" width="20%"></img>
+      <img src="/hover_over_cards.svg" className="absolute bottom-20 left-170" width="20%"></img>
       <main className="h-screen w-screen flex flex-row items-center justify-center bg-gradient-to-b from-white to-[#E3E0E0] via-white [background-position:0%]">
        
       <div className="flex flex-col items-center justify-left w-[50%] gap-8">
-        <h1 className="flex items-center justify-center text-6xl h-[10%] w-[75%] text-black font-extrabold text-left animate-gradient-x">
+        <h1 className="flex items-center text-6xl w-[75%] h-[100%] text-black font-extrabold text-left animate-gradient-x">
           good news.<br />your new favourite spot is right around the corner.
         </h1>
         <span className="w-[75%] text-left text-2xl gap-50">
