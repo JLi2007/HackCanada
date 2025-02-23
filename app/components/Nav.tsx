8f9dcd757b84854a3d08b758ec26acdf576c79b2
JLi2007 <LiJames9532@gmail.com>
2025-02-23 08:32:40 -0500

    branc

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -57,15 +57,15 @@ function Nav() {
               >
                 profile
               </a>
-            </li>
+            </li> */}
             <li>
               <a
-                href="#"
+                href="/dashboard"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                leaderboard
+                dashboard
               </a>
-            </li> */}
+            </li>
             {/* Sign In/Out Button */}
             <li>
               {status === "loading" ? (
